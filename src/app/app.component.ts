c4150562666a811d741ac4dc43b50b7b861687f2
marek <mc@amcbanking.com>
2019-10-10 16:01:43 +0200

    Third cmmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rest-front-app';
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { name: string; quantity: number }) {
+    this.http.post(
+      'http://localhost:8080/list/add',
+       postData
+       ).subscribe(responseData => {
+         console.log(responseData);
+       });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
 }
